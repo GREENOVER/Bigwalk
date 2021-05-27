2f842ba3be76e49d44557d56dfa269b22fc4550e
GREEN <humains29@gmail.com>
2021-05-28 08:06:35 +0900

    docs: 프로젝트 파일 및 빌드 타겟 설정

---
+++ Bigwalk/BigwalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigwalkApp.swift
+//  Bigwalk
+//
+//  Created by 조찬우 on 2021/05/28.
+//
+
+import SwiftUI
+
+@main
+struct BigwalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
