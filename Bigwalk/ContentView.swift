e62c52dd0bcb6221d42c6faa8edba2d9330af4cb
GREEN <humains29@gmail.com>
2021-05-28 17:25:05 +0900

    feat: 공개형/기업형 피커 추가

--- Bigwalk/ContentView.swift
+++ Bigwalk/ContentView.swift
@@ -2,13 +2,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 10) {
+            HStack {
+                Picker(selection: .constant(1), label: Text("Picker")) {
+                    Text("공개형").tag(1)
+                    Text("기업형").tag(2)
+                }.pickerStyle(SegmentedPickerStyle()).padding(.horizontal)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
