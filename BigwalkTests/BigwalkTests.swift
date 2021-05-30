6c3df7d08dbb7648694049cc5d81e787681862da
GREEN <humains29@gmail.com>
2021-05-30 11:46:21 +0900

    [#5] feat: Unit Test를 통한 Mock 데이터 디코딩 테스트 메서드 구현

---
+++ BigwalkTests/BigwalkTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+@testable import Bigwalk
+
+class BigwalkTests: XCTestCase {
+    
+    func testDecodeMockData() throws {
+        let data: [Campaign] = JSONDecode.shared.loadJSON("CampaignMock.json")!
+        XCTAssertEqual(data[0].title, "챌린지 테스트")
+        XCTAssertEqual(data[0].ratio, 0)
+        XCTAssertEqual(data[59].title, "나다의 #일일일만보")
+        XCTAssertEqual(data[59].ratio, 2956)
+    }
+
+}
