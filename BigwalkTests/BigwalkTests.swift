3a2ec9c9828ace25319779e6cf3e2eb0d13b360a
GREEN <humains29@gmail.com>
2021-05-30 11:52:12 +0900

    [#5] feat: 참여율 순 데이터 정렬 유닛 테스트 메서드 구현

--- BigwalkTests/BigwalkTests.swift
+++ BigwalkTests/BigwalkTests.swift
@@ -10,5 +10,12 @@ class BigwalkTests: XCTestCase {
         XCTAssertEqual(data[59].title, "나다의 #일일일만보")
         XCTAssertEqual(data[59].ratio, 2956)
     }
+    
+    func testArrayCampaign() throws {
+        let data: [Campaign] = JSONDecode.shared.loadJSON("CampaignMock.json")!
+        let ratioData = data.sorted{$0.ratio < $1.ratio}
+        XCTAssertEqual(ratioData[0].ratio, 0)
+        XCTAssertEqual(ratioData[59].ratio, 2956)
+    }
 
 }
